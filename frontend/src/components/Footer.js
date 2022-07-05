05e86d4bd8411fbf80b10c72caa031f12f1e9b95
Rayan <53921750+binelias@users.noreply.github.com>
2022-07-05 23:35:37 +0800

    adding to shopping cart

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            Copyright &copy; RobinsonShop
+            Copyright &copy; {(new Date().getFullYear())} RobinsonShop
           </Col>
         </Row>
       </Container>
