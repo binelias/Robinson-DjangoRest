bb106b9e13a56f591565ebcd27f1bbf9e1646f29
Rayan <53921750+binelias@users.noreply.github.com>
2022-07-05 11:22:51 +0800

    frontend

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Product from '../components/Product';
+
+import products from '../products';
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(products => (
+          <Col key={products._id} sm ={12} md={6} lg={4} xl={3}>
+            <Product product={products} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  )
+}
+
+export default HomeScreen
